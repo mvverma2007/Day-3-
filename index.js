e5afdfb73f269267b753f3b3534006f3e39bab00
mvverma2007 <mv.verma2007@gmail.com>
2025-12-17 12:58:18 +0530

    shopping website

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-    return res.send("Hi Amy How are you?");
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 //listen
 
